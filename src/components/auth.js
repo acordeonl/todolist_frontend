7430bf3e666c7d5872478b9c67b31333f0c69f9c
acordeonl <acordeonlwebmaster@gmail.com>
2019-03-20 20:55:48 -0500

    add login/signup form

--- src/components/auth.js
+++ src/components/auth.js
@@ -5,36 +5,49 @@ import Button from '@material-ui/core/Button';
 import { layoutStyles } from '../styles'
 
 class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.action = ''
+  }
+
+  handleSubmit ( event ) {
+    event.preventDefault();
+    const formData = new window.FormData(event.target)
+    console.log(this.action,formData.get('email'),formData.get('password'));
+  }
   render() {
     return (<div>
       <Paper elevation={1}>
-        <div className='wrapper'>
+        <form className='wrapper' onSubmit={this.handleSubmit.bind( this )}>
           <TextField
-            id="outlined-email-input"
+            id="email"
             label="Email"
             type="email"
             name="email"
             autoComplete="email"
+            onChange={evt => { this.email = evt.target.value }}
             margin="normal"
             variant="outlined"
           />
           <TextField
-            id="outlined-password-input"
+            id="password"
+            name="password"
             label="Password"
             type="password"
             autoComplete="current-password"
+            onChange={evt => { this.password = evt.target.value }}
             margin="normal"
             variant="outlined"
           />
           <div className='spaceBetween buttons'>
-            <Button  color='primary' size='large' variant='contained'>
+            <Button onClick={() => { this.action = 'login' }} type='submit' color='primary' size='large' variant='contained'>
               LOG IN
-            </Button>
-            <Button  color='primary' size='large' variant='contained'>
+              </Button>
+            <Button onClick={() => {this.action = 'singUp'}}  type='submit' color='primary' size='large' variant='contained'>
               SIGN UP
-            </Button>
+              </Button>
           </div>
-        </div>
+        </form>
       </Paper>
       <style jsx>{layoutStyles}</style>
       <style jsx>{`
